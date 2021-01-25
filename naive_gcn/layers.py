c158dd767f8a8c8fed4d6f4beea9fca90b8e852c
xiaoyunw <xiaoyunw@nvidia.com>
2021-01-25 20:12:18 +0000

    changed spmm in layers.py to egonets

---
+++ naive_gcn/layers.py
@@ -0,0 +1,64 @@
+import math
+
+import torch
+
+from torch.nn.parameter import Parameter
+from torch.nn.modules.module import Module
+import networkx as nx
+
+
+class GraphConvolution(Module):
+    """
+    Simple GCN layer, similar to https://arxiv.org/abs/1609.02907
+    """
+
+    def __init__(self, in_features, out_features, bias=True):
+        super(GraphConvolution, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = Parameter(torch.FloatTensor(in_features, out_features))
+        if bias:
+            self.bias = Parameter(torch.FloatTensor(out_features))
+        else:
+            self.register_parameter('bias', None)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        stdv = 1. / math.sqrt(self.weight.size(1))
+        self.weight.data.uniform_(-stdv, stdv)
+        if self.bias is not None:
+            self.bias.data.uniform_(-stdv, stdv)
+
+    def forward(self, input, G):
+        # need to change this part
+        # input is X, adj is A, self.weight is w
+        support = torch.mm(input, self.weight)
+        # first nx with support
+        #adj_mat = adj.to_dense().cpu().numpy()
+        #G = nx.from_numpy_matrix(adj_mat)
+        # get 1 hop ego net
+        output1 = [] 
+        for node in G.nodes():
+            ego_Net = nx.ego_graph(G,node)
+            target = [item[1] for item in list(ego_Net.edges(node))]
+            #print ("node", node)
+            #print ("target", target)
+            #for (u, v) in ego_Net.edges(node):
+                #print(u,v)
+                #print(ego_Net.get_edge_data(v,u))
+            output_vect = sum(support[target])/len(target)
+            output1 += [output_vect]
+            output2 = torch.stack(output1, dim =0)
+
+        #print("output2", output2.size())
+        #output = torch.spmm(adj, support)
+        #print("output", output.size())
+        if self.bias is not None:
+            return output2 + self.bias
+        else:
+            return output2
+
+    def __repr__(self):
+        return self.__class__.__name__ + ' (' \
+               + str(self.in_features) + ' -> ' \
+               + str(self.out_features) + ')'
